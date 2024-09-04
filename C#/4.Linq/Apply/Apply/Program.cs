ad73c3776fb5868a7cc315c79a0ab1c31e5c0ac7
FawzyFathi01 <fawzyfathi6@gmail.com>
2024-09-04 15:18:31 +0300

    Update  Linq

--- C#/4.Linq/Apply/Apply/Program.cs
+++ C#/4.Linq/Apply/Apply/Program.cs
@@ -3,11 +3,41 @@ namespace Apply
 {
     internal class Program
     {
+        static List<Employee> emps = (List<Employee>) Repository.LoadEmployees();
+        static List<Department> deprts = (List<Department>)Repository.LoadDepartment();
+
         static void Main(string[] args)
         {
+            Group_by_Department();
 
-            Console.WriteLine("Fawzy Ready To Day");
+
 
         }
+    
+
+        static void Group_by_Department()
+        {
+            var res = emps.GroupBy(d => d.DepartmentId);
+
+            foreach (var item in res)
+            {
+                //Console.WriteLine(item.Key);
+                //foreach (var item1 in item)
+                //{
+                //    Console.WriteLine(item1.FullName);
+                //}
+
+                item.Print($"Depart ({item.Key})");
+
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
