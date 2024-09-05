f0e874d70b586a346d2b0964efc1b60f56b0100a
FawzyFathi01 <fawzyfathi6@gmail.com>
2024-09-05 09:35:52 +0300

    Add EF Core To Task on ADO.NET

--- C#/5.EF_Core/DEPI_Task_1_ADO/DEPI_Task_1_ADO/Program.cs
+++ C#/5.EF_Core/DEPI_Task_1_ADO/DEPI_Task_1_ADO/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Apply_2
+﻿namespace DEPI_Task_1_ADO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
